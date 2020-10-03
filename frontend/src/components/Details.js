ad8f453aee3d016b2562d86b6994e4b5b064d569
akshitgarg4 <akshit2000garg@gmail.com>
2020-10-03 17:53:51 +0530

    Login/Logout Successful

--- frontend/src/components/Details.js
+++ frontend/src/components/Details.js
@@ -1,29 +1,28 @@
 import React, {Component} from "react";
 import {Redirect} from "react-router-dom";
-import {Link} from "react-router-dom";
 import {connect} from "react-redux";
 
 class Details extends Component {
 
     render() {
-        const {inProgress, error, isLoggedIn} = this.props.auth;
+        const { isLoggedIn} = this.props.auth;
 
         //so that not logged in user don't sees the details page
-        {
-            /*
+        
+            
         if (!isLoggedIn) {
           return <Redirect to="/" />;
         }
-    */
-        }
+    
+        
 
         return (
             <div className="Main-Page">
                 <div className="choice">
-                    <div class="details">
+                    <div className="details">
                         <button>FORM</button>
                     </div>
-                    <div class="chatbot">
+                    <div className="chatbot">
                         <button>CHAT BOT</button>
                     </div>
                 </div>
