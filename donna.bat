7a071d1512f87f751a0aace992410f8a7bf36416
Gaganpreet Khurana <giftykhurana0@gmail.com>
2020-10-12 14:50:22 +0530

    Added batch files to install requirements

--- donna.bat
+++ donna.bat
@@ -1,10 +1,10 @@
-title "DONNA"
-echo "Starting Servers"
+title DONNA
+echo Starting Servers
 cd frontend
 start npm start
 cd ..
-echo "Frontend Server Started"
-echo "Starting Backend Server"
+echo Starting Frontend Server
+echo Starting Backend Server
 call backend\venv\Scripts\activate.bat
 python backend\manage.py runserver
 pause
---
+++ install_requirements.bat
@@ -0,0 +1,14 @@
+title DONNA
+echo Installing requirements
+cd frontend
+start npm install
+cd ..
+echo Installing Frontend Requirements
+python -m pip install --upgrade pip
+python -m pip install virtualenv
+python -m venv backend/venv
+call backend\venv\Scripts\activate.bat
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+echo Backend Requirements Installed
+pause
