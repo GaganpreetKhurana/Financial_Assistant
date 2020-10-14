2a5f8919cb2d5cd9c5e66bd2e8a404bbe8614ae9
Gaganpreet Khurana <giftykhurana0@gmail.com>
2020-10-14 20:41:04 +0530

    endpoints's added for amazon wishlist and stock tracker

--- backend/chatbot/urls.py
+++ backend/chatbot/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 app_name = "chatbot"
 urlpatterns = [
-    ##path("chat", views.button),
-    ##path("output", views.ChatAnsRequest, name="ChatAnsRequest"),
+    # path("chat", views.button),
+    # path("output", views.ChatAnsRequest, name="ChatAnsRequest"),
     path("external/", views.external, name="bot"),
     path("old/", views.get_previous_chats, name="previous_chats"),
+    path("stock_list/", views.stock_list_view, name="stock_list"),
+    path("wishlist/", views.amazon_wishlist_view, name="wishlist"),
 ]
