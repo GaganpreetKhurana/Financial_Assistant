75c19e740fcf773e34a3aa25e5774dd179e2a3bd
akshitgarg4 <akshit2000garg@gmail.com>
2020-10-15 12:46:21 +0530

    Stock list displayed successfully

--- frontend/src/components/StockPage.js
+++ frontend/src/components/StockPage.js
@@ -4,6 +4,7 @@ import {fetchStocklist} from "../actions/pages";
 
 
 
+
 class StockPage extends Component {
 
      //fetch stocklist
@@ -12,10 +13,32 @@ class StockPage extends Component {
     }
     render() {
     const {stocklist}=this.props.details;
-    console.log("#########",stocklist);
+    console.log(stocklist);
+
         return (
             <div>
-                Stock Page
+               <h2><br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Stocklist</h2>
+               <div className="form-box2">
+                <div className="wishlist-entry">
+                        <div className="numbs headers"> No. </div>
+                        <div className="stocks headers">Category</div> 
+                        <div className="owned headers">Owned Price &nbsp;(Rs.)</div>   
+                        <div className="price headers">Current Price &nbsp;(Rs.)</div>   
+                
+                </div>
+               {stocklist.length === 0 && <div><br></br><h2>No Stocklist to display</h2></div>}
+               <div className="transactions-box">
+               {stocklist.map((entry,index)=>(
+                <div className="transaction-entry" key={`entry.createdAt-${index}`}>
+                   <div className="numbs"> {index+1+"."} </div>
+                   <div className="stocks ">{entry.stock}</div>  
+                   <div className="owned">&nbsp;&nbsp;&nbsp;&nbsp;{entry.owned}</div> 
+                   <div className="price">&nbsp;&nbsp;&nbsp;&nbsp;{entry.current_price}</div>  
+                </div>
+                    ))
+                }
+                </div>
+                </div>
             </div>
         );
     }
