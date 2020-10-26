da5a8708279fc7dc20300ca0d7fa8c2a15e1e60c
ShayanYaseen <shayan.yaseen1@gmail.com>
2020-10-26 20:37:53 +0530

    revert navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -19,27 +19,19 @@ class Navbar extends Component {
         const {auth} = this.props;
         return (
             <div className="navbar">
-                <AppBar position="static" color="transparent">
-                <Toolbar>
-                <Grid container  justify="space-between">
-                <Grid>
                 <div className="nav-left">
                 <Typography type="title">
                     <Link to="/">DONNA</Link>
                 </Typography>
                 </div>
-                </Grid>
-                <Grid item xs={9} align="center"><div id="google_translate_element"></div></Grid>
-                <Grid><div className="nav-right">
-                    {!auth.isLoggedIn && (<Button variant="contained" color="secondary" ><Link to="/login">Login</Link></Button>)}
+                <div id="google_translate_element"></div>
+                <div className="nav-right">
+                    {!auth.isLoggedIn && (<Button variant="contained" color="primary" ><Link to="/login">Login</Link></Button>)}
                     {auth.isLoggedIn && (<button className="bttn-home"> <Link to="/profile">{auth.user.username} </Link></button>)}
-                    {!auth.isLoggedIn && (<Button variant="contained" color="secondary" ><Link to="/sign-up">Sign Up</Link></Button>)}
-                    {auth.isLoggedIn && (<Button variant="contained" color="secondary" onClick={this.logout}>Log Out</Button>)}
-                </div></Grid>
-                </Grid>
-                </Toolbar>
-                </AppBar>
-
+                    &nbsp
+                    {!auth.isLoggedIn && (<Button variant="contained" color="primary" ><Link to="/sign-up">Sign Up</Link></Button>)}
+                    {auth.isLoggedIn && (<Button variant="contained" color="primary" onClick={this.logout}>Log Out</Button>)}
+                </div>
             </div>
         );
     }
