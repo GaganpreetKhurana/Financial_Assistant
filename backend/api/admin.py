0e505ef7b2abd1771096b888f14a6877c681342e
Gaganpreet Khurana <giftykhurana0@gmail.com>
2020-10-27 18:49:36 +0530

    Edited Comments backend/api/admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -5,20 +5,26 @@ from .models import *
 
 # Register your models here.
 class DetailAdmin(admin.ModelAdmin):
+    """
+    Admin Model for Detail Model
+    """
     readonly_fields = ['user', 'date_created', 'savings', 'totalExpenditure', 'totalTransactions']
 
     class Meta:
         model = Detail
 
 
-admin.site.register(Detail, DetailAdmin)
+admin.site.register(Detail, DetailAdmin)  # Register Detail Admin with admin
 
 
 class TransactionAdmin(admin.ModelAdmin):
+    """
+    Admin Model for Transaction Model
+    """
     readonly_fields = ['time', 'user', 'details', 'last_updated']
 
     class Meta:
         model = Transaction
 
 
-admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(Transaction, TransactionAdmin)  # Register Transaction Admin to admin
