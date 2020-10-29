865ced91a521d7f4966473da739c37f72482f193
akshitgarg4 <akshit2000garg@gmail.com>
2020-10-29 16:46:04 +0530

    Speed of animation slowed

--- frontend/src/components/Background.js
+++ frontend/src/components/Background.js
@@ -2,8 +2,19 @@ import React,{Component} from 'react'
 import ParticlesBg from 'particles-bg'
 class Background extends Component{
     render(){
+        let config = {
+            num: [6],
+            rps: 1.2,
+            radius: [10,100],
+            life: [1.5, 3],
+            v: [0.4],
+            scale: [0.8,0.6],
+            position: "all", 
+            color: ["random"],
+            
+          };
         return(
-            <ParticlesBg num={25} type='circle' bg={true} zIndex="-100"/>
+            <ParticlesBg type="custom" config={config} bg={true}/>
         )
     }
 }
