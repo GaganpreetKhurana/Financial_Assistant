bc4d090b6ce195b33232618a654e9fea80b58e10
Gaganpreet Khurana <giftykhurana0@gmail.com>
2020-11-20 13:50:50 +0530

    Stock view edited

--- backend/chatbot/views.py
+++ backend/chatbot/views.py
@@ -105,7 +105,7 @@ def stock_interact_view(request):
         :return: Response from Stock Tracker
         """
     try:
-        if request.data.get("credit"):
+        if not request.data.get("credit"):
             response = SellStock(request.data.get("amount"),
                                  request.data.get('description').split()[0],
                                  request.user.id)
