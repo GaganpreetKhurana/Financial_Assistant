c3e0795c0f6268b21e3c59f1d67aa4ee0df7174f
Gaganpreet Khurana <54024846+GaganpreetKhurana@users.noreply.github.com>
2022-02-24 18:43:03 +0530

    Updates

---
+++ precache-manifest.8391177d36cbca3ce5a371ae13892a1c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f1f87a8eca5f113099166041fcd421",
+    "url": "/Financial_Assistant/index.html"
+  },
+  {
+    "revision": "6805152337f02cab9ea2",
+    "url": "/Financial_Assistant/static/css/2.c7661d7b.chunk.css"
+  },
+  {
+    "revision": "8e50b197c1b58b015adb",
+    "url": "/Financial_Assistant/static/css/main.389eb942.chunk.css"
+  },
+  {
+    "revision": "6805152337f02cab9ea2",
+    "url": "/Financial_Assistant/static/js/2.59dda259.chunk.js"
+  },
+  {
+    "revision": "a0798d2cabccbdf86134018a4a3a70d6",
+    "url": "/Financial_Assistant/static/js/2.59dda259.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e50b197c1b58b015adb",
+    "url": "/Financial_Assistant/static/js/main.32fd6d22.chunk.js"
+  },
+  {
+    "revision": "5cd0ad512f84ff1265a8",
+    "url": "/Financial_Assistant/static/js/runtime-main.63a85172.js"
+  },
+  {
+    "revision": "8db48c22f237f5c5395e8f964b075bf8",
+    "url": "/Financial_Assistant/static/media/amazon.8db48c22.png"
+  },
+  {
+    "revision": "914adc5d54dfe35751b3d9fd6352373f",
+    "url": "/Financial_Assistant/static/media/chat.914adc5d.png"
+  },
+  {
+    "revision": "bced29908d2a673ec4bc41b5ba43d58a",
+    "url": "/Financial_Assistant/static/media/stocks.bced2990.png"
+  },
+  {
+    "revision": "c11318acf44853b9efa0e2a9d2718c2f",
+    "url": "/Financial_Assistant/static/media/transact.c11318ac.png"
+  },
+  {
+    "revision": "99d6c27bf1704d90298d6a166577ec3d",
+    "url": "/Financial_Assistant/static/media/view.99d6c27b.png"
+  },
+  {
+    "revision": "335a95f8316241ab1a7a878389f42a6b",
+    "url": "/Financial_Assistant/static/media/visualizations.335a95f8.png"
+  }
+]);
