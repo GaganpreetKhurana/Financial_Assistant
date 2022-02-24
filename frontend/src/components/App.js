0e4bd8967838cbd520f0c1fd204532f66285ccee
Gaganpreet Khurana <54024846+GaganpreetKhurana@users.noreply.github.com>
2022-02-24 19:59:41 +0530

    Added to Github Pages

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -38,7 +38,7 @@ class App extends Component {
 
     render() {
         return (
-            <Router>
+            <Router basename={process.env.PUBLIC_URL}>
                 <div>
                     <Navbar/>
                     <Switch>
